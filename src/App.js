a8fe6442bf364f3685ffb00ed1ffceca993e2311
laraanna <lara@laraannawagner.com>
2018-02-20 12:55:57 +0100

    Set render Canvas Function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import DrawBoard from './components/DrawBoard'
-import Circle from './containers/circle';
+import Circle from './containers/Circle';
 import './App.css';
 
 class App extends Component {
--- src/containers/Circle.js
+++ src/containers/Circle.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import './Circle.css'
 
 
 let mapStateToProps    = (state)    => state;
@@ -10,6 +11,17 @@ class Circle extends Component {
     this.refs.canvas.width = 400;
     this.refs.canvas.height = 400;
     this.ctx = this.refs.canvas.getContext('2d');
+
+    this.renderCanvas(this.props);
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.renderCanvas(nextProps)
+  }
+
+  renderCanvas(props){
+    this.ctx.fillStyle= '#000';
+    this.ctx.fillRect(0,0,this.refs.canvas.width, this.refs.canvas.height)
   }
   render(){
     return(
