b2e5e0296fb70dd84aecfc3cf58c51b8ec8f3456
laraanna <lara@laraannawagner.com>
2018-02-20 13:37:27 +0100

    Set up ToolbarItem Component

---
+++ src/components/ToolbarItem.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class ToolbarItem extends Component {
+  render(){
+    return(
+      <div className="ToolbarItem">
+      </div>
+    )
+  }
+}
+
+export default ToolbarItem;
